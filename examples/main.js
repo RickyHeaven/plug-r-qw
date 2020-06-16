e6bca17fbc345b1d81c6e46c8680830525a095cd
ricky <zhangqingcq@foxmail.com>
2020-06-16 15:16:21 +0800

    添加 iview lodash iview-area less moment nprogress qs sweetalert wangeditor

--- examples/main.js
+++ examples/main.js
@@ -2,12 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import iView from 'view-design'
+import 'view-design/dist/styles/iview.css'
+import './global/iViewTheme.less'
+import _ from "lodash"
 import plugRQw from '../src/index.js'
 
 Vue.config.productionTip = false
 
+Vue.use(iView)
 Vue.use(plugRQw)
 
+window._ = _
 
 new Vue({
   router,
---
+++ src/components/testViewDesign.vue
@@ -0,0 +1,13 @@
+<template>
+  
+</template>
+
+<script>
+  export default {
+    name: "testViewDesign"
+  }
+</script>
+
+<style scoped>
+
+</style>
