b3595d0e690d8f6ae7917e737c666b26df387b62
ricky <zhangqingcq@foxmail.com>
2020-06-19 16:23:07 +0800

    引入 gzip 暴露source map 第三方依赖不打包在库中 需单独安装

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,90 @@
+const compressionPlugin = require("compression-webpack-plugin")
+
 module.exports = {
-  productionSourceMap: false,
-  pages:{
-    index:{
-      entry:'examples/main.js',
-      template:'public/index.html',
-      filename:'index.html'
+  productionSourceMap: true,
+  pages: {
+    index: {
+      entry: 'examples/main.js',
+      template: 'public/index.html',
+      filename: 'index.html'
+    }
+  },
+  configureWebpack: config =>{
+    if(process.env.NODE_ENV === 'production'){
+      return {
+        output: {
+          libraryExport: 'default'
+        },
+        externals: {
+          'view-design': {
+            root: 'ViewUI',
+            commonjs: 'view-design',
+            commonjs2: 'view-design',
+            amd: 'view-design'
+          },
+          'sweetalert': {
+            root: 'swal',
+            commonjs: 'sweetalert',
+            commonjs2: 'sweetalert',
+            amd: 'sweetalert'
+          },
+          'lodash': {
+            root: '_',
+            commonjs: 'lodash',
+            commonjs2: 'lodash',
+            amd: 'lodash'
+          },
+          'axios': {
+            root: 'axios',
+            commonjs: 'axios',
+            commonjs2: 'axios',
+            amd: 'axios'
+          },
+          'echarts': {
+            root: 'echarts',
+            commonjs: 'echarts',
+            commonjs2: 'echarts',
+            amd: 'echarts'
+          },
+          'moment': {
+            root: 'moment',
+            commonjs: 'moment',
+            commonjs2: 'moment',
+            amd: 'moment'
+          },
+          'wangeditor': {
+            root: 'wangeditor',
+            commonjs: 'wangeditor',
+            commonjs2: 'wangeditor',
+            amd: 'wangeditor'
+          },
+          'js-cookie': {
+            root: 'Cookies',
+            commonjs: 'js-cookie',
+            commonjs2: 'js-cookie',
+            amd: 'js-cookie'
+          },
+          'iview-area': {
+            root: 'iviewArea',
+            commonjs: 'iview-area',
+            commonjs2: 'iview-area',
+            amd: 'iview-area'
+          },
+          'vue-amap': {
+            root: 'VueAMap',
+            commonjs: 'vue-amap',
+            commonjs2: 'vue-amap',
+            amd: 'vue-amap'
+          }
+        },
+        plugins:[
+          new compressionPlugin({
+            test:/\.js$|\.html$|\.css/, //匹配文件名
+            threshold:10240, //对超过10kb的数据进行压缩
+            deleteOriginalAssets: false //是否删除原文件
+          })
+        ]
+      }
     }
   },
   chainWebpack: config => {
