318241e110fbb78b365bf97a860b0bc1dd30b198
ricky <zhangqingcq@foxmail.com>
2020-10-27 15:26:32 +0800

    升级editor组件，改为wangeditor@4.0.6版本，新增了一些菜单（之前定位为简洁轻量化，但随着需求增加，不得不放出绝大部功能）

--- src/components/editor/editor.vue
+++ src/components/editor/editor.vue
@@ -6,8 +6,7 @@
 </template>
 
 <script>
-  import Editor from 'wangeditor'
-  import 'wangeditor/release/wangEditor.min.css'
+  import E from 'wangeditor'
   import {oneOf} from "../../methods/functionGroup"
   import xss from 'xss'
 
@@ -58,30 +57,39 @@
       }
     },
     mounted() {
-      this.editor = new Editor(`#${this.editorId}`)
-      this.editor.customConfig.zIndex = 100
-      this.editor.customConfig.menus = [
+      this.editor = new E(`#${this.editorId}`)
+      this.editor.config.zIndex = 100
+      this.editor.config.menus = [
         'head',// 标题
         'bold',// 粗体
         'fontSize',// 字号
+        'fontName',//字体
         'italic',// 斜体
         'underline', // 下划线
+        'strikeThrough',//删除线
+        'indent',//缩进
+        'lineHeight',//行高
         'foreColor',// 文字颜色
         'backColor',// 背景颜色
         'link',// 插入链接
+        'list',//列表
         'justify',// 对齐方式
+        'quote',//引用
+        'emoticon',//表情符
         'image',// 插入图片
+        'video',//视频
         'table',  // 表格
+        'splitLine',//分割线
         'undo',  // 撤销
         'redo'// 重复
       ]
-      this.editor.customConfig.onchange = (html) => {
+      this.editor.config.onchange = (html) => {
         let text = this.editor.txt.text()
         let htmlT = xss(html)
         this.$emit('input', this.valueType === 'html' ? htmlT : text)
         this.$emit('on-change', this.valueType === 'html' ? htmlT : text)
       }
-      this.editor.customConfig.onchangeTimeout = this.changeInterval
+      this.editor.config.onchangeTimeout = this.changeInterval
       // create这个方法一定要在所有配置项之后调用
       this.editor.create()
       if(this.value){
