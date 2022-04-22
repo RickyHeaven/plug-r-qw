8ffeaa09e8f6629f85282c4b20db780cba5674d2
ricky <zhangqingcq@foxmail.com>
2022-04-22 17:29:35 +0800

    下拉选择更多 选择框 示例 添加说明文字

--- examples/components/selectScrollMoreEX.vue
+++ examples/components/selectScrollMoreEX.vue
@@ -2,6 +2,7 @@
   <div class="examplePageL vh100">
     <showReadMe/>
     <toHome/>
+    <div style="margin: 20px 0">请注意：该示例因为要请求接口，所以请执行“npm run node-serve”开启node-serve后方可正常演示</div>
     <select-scroll-more v-model="value" url="/node-serve/select-scroll-more" :searchData="{size:7}" @on-change="handleChange" style="width: 250px"/>
     <p class="outJ">值： <span>{{value}}</span> </p>
   </div>
