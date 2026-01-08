e3d2fb9ba71b67184ef5856b94aa00cbfb32a315
RickyHeaven <zhangqingcq@foxmail.com>
2026-01-08 16:19:51 +0800

    1.tablePrint方法更新，适配当columns中只有width没有minWidth的场景。

--- src/utils/tablePrint/index.js
+++ src/utils/tablePrint/index.js
@@ -61,9 +61,13 @@ function init(router) {
 
 function columnsHandle(item) {
 	if (myTypeof(item) === 'Object') {
-		item.width = item.minWidth || 100
+		if (!item.hasOwnProperty('width')) {
+			item.width = item.minWidth || 100
+		}
+		if (item.hasOwnProperty('minWidth')) {
+			delete item.minWidth
+		}
 		item.sortable = false
-		delete item.minWidth
 		delete item.__id
 		item.resizable = true
 		if (item.children && myTypeof(item.children) === 'Array') {
