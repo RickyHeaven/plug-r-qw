6e6aec4d006f97b9c58a7737af595df36636700a
RickyHeaven <zhangqingcq@foxmail.com>
2026-07-23 16:49:56 +0800

    1.inputMap示例微调。

--- examples/components/inputMapEX.vue
+++ examples/components/inputMapEX.vue
@@ -20,15 +20,11 @@
 			return {
 				value: {
 					name: '光电园(地铁站)',
-					address: '6号线',
-					district: '重庆市渝北区',
 					lng: 106.499401,
 					lat: 29.616077
 				},
 				value2: {
 					name: '光电园(地铁站)',
-					address: '6号线',
-					district: '重庆市渝北区',
 					lng: 106.499401,
 					lat: 29.616077
 				}
